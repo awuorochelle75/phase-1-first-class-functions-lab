f6711feeeeb246c02c4249d5cd1a8cd54671db5e
Rochelle Awuor <awuorochelle75@gmail.com>
2025-03-08 16:50:30 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+const drivers=['Antonia','Nuru','Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2)
+   
+};
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+const selectDifferentDrivers=function (arrayOfDrivers,funct){
+    return funct(arrayOfDrivers)
+};
+
+
+
+
+const createFareMultiplier = function (multiplier){
+    return function(fare){
+        return fare * multiplier
+    };
+};
+
+const fareDoubler= createFareMultiplier(2);
+const fareTripler= createFareMultiplier(3)
+
+
+    
+
+
+
+
+  
+
+
+
+
+  
+  
